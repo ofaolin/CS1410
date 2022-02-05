58d9a13c287d40336fc7ad6d52e2338c0e7fae00
Tyler Phalen <tyler.phalen@protonmail.com>
2022-02-05 06:49:44 -0700

    Initial Commit

---
+++ CS1410/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CS1410
+//
+//  Created by Tyler Phalen on 2/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
